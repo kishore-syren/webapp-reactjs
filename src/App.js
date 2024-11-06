35ddb8560c861904826e7873c3888b1431f6b0c3
kiranbalaji71 <kiran19701@gmail.com>
2024-11-06 12:00:57 +0530

    webpage setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Photo from "./Assets - kishore/IMG-20210520-WA0003.jpg";
+import SingAudio from "./Assets - kishore/sing.aac";
+import SoundAudio from "./Assets - kishore/sound.mp3";
+import "./App.css";
 
 function App() {
+  // Create audio instances for each sound file
+  const singAudio = new Audio(SingAudio);
+  const soundAudio = new Audio(SoundAudio);
+
+  // Play the audio from the start
+  const playAudio = (audio) => {
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="story">
+        <div className="name">Kishore</div>
+        <img src={Photo} alt="kishore" />
+        <div className="buttons">
+          <button onClick={() => playAudio(singAudio)}>Sing</button>
+          <button onClick={() => playAudio(soundAudio)}>Sound</button>
+        </div>
+      </div>
     </div>
   );
 }
